559c424f853dae76e45ba9b7c853f1a0cf068dbd
KrishPatel-hub <krishp200718@gmail.com>
2025-08-13 16:25:53 +0530

    Add files via upload

---
+++ evenOdd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class evenOdd {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.println(n+" is even");
+        }
+        else{
+            System.out.println(n+" is odd");
+        }
+    }   
+}
